5d0d4b1760b455cd16fa37394166a3b530f994a4
cugblack <18202794429@163.com>
2018-11-23 04:09:54 -0800

    更新依赖软件

--- script/docker-for-k8s-centos7.sh
+++ script/docker-for-k8s-centos7.sh
@@ -6,11 +6,12 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 # Step 3: 更新并安装Docker-CE
 yum makecache fast
-yum -y install docker-ce
-# Step 4: 开启Docker服务
+#依赖包docker-ce-selinux
+yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch.rpm
+#安装
+yum install docker-ce-17.03.2.ce-1.el7.centos -y 
+# Step 4: 加入开机启动Docker服务
 systemctl enable docker
-
+#启动docker
 systemctl start docker
-
-swapoff -a
 
